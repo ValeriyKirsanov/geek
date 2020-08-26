15f5c3ce00bbcef99e41cd7ff8738571d7f7e242
ValeriyKirsanov <70278649+ValeriyKirsanov@users.noreply.github.com>
2020-08-26 18:48:49 +0300

    Add files via upload

---
+++ exemp6.py
@@ -0,0 +1,7 @@
+n = int(input("введите число от 1 до 9 __"))
+first = str(n)
+second = first + first
+third = first + first + first
+print(first, "+", second, "+", third)
+itogo = int(first) + int(second) + int(third)
+print('=', itogo)
